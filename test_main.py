502e3e1da24d31eaa5e5152555f36e08eb98a693
ratajalbalawi <ralbalaw@emich.edu>
2024-04-21 10:06:18 +0000

    93% coverage:                                                                                        issue #7

--- test_main.py
+++ test_main.py
@@ -1,24 +1,29 @@
-import pytest
 from fastapi.testclient import TestClient
 from main import app
+import pytest
 from unittest.mock import MagicMock
+
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
+
 @pytest.fixture
 def course_manager():
     return MagicMock()
+
 
 @pytest.fixture
 def user_manager():
     return MagicMock()
 
+
 def test_welcome(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.text.strip('"') == "Mini Canvas"
+
 
 def test_create_a_course(client, user_manager, course_manager):
     user_manager.create_a_user = MagicMock(return_value=None)
@@ -26,15 +31,16 @@ def test_create_a_course(client, user_manager, course_manager):
     assert response.status_code == 200
     assert response.json()
 
+
 def test_import_students(client, course_manager):
-    course_manager.create_a_course = MagicMock(return_value=None)
+    course_manager.find_a_course = MagicMock(return_value=MagicMock(import_students=MagicMock()))
     response = client.put("/courses/1/students", json={"student_id_list": [3, 4]})
-    assert response.status_code == 200 
+    assert response.status_code == 200
     assert response.json()
+
 
 def test_course_not_found(client, course_manager):
     course_manager.find_a_course = MagicMock(return_value=None)
     response = client.put("/courses/2/students", json={"student_id_list": [3, 4]})
     assert response.status_code == 404
     assert response.json() == {"detail": "Course 2 not found"}
-
